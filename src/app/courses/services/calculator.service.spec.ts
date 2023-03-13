f57ba47f8add6bc571cae394452da11d532f654a
Felipe Jhordan <44248690+FelipeJhordan@users.noreply.github.com>
2023-03-12 22:52:04 -0300

    test: ensure calcultor service works correctly

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,43 @@
+import { templateSettings } from "cypress/types/lodash";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+import { TestBed } from '@angular/core/testing'
+
+describe('CalculatorService', () => {
+    let calculator: CalculatorService
+    let loggerSpy: LoggerService
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"])
+        
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {
+                    provide: LoggerService,
+                    useValue: loggerSpy
+                }
+            ]
+        })
+
+        calculator = TestBed.inject(CalculatorService)
+    })
+
+    it('should add two numbers', () => {
+        
+    
+
+        const result = calculator.add(2, 2)
+
+        expect(result).toBe(4)
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1)
+    })
+
+    it('should substract two numbers', () => {
+
+      
+
+        const result = calculator.subtract(2, 2)
+
+        expect(result).toBe(0, "unexpected substration result.")    
+    })
+});
